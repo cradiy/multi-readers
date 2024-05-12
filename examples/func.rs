7815f8505e4f88ea526821e0cdfad1bac64f3d11
KByys <3138859984@qq.com>
2024-05-12 23:40:20 +0800

    update example

--- examples/func.rs
+++ examples/func.rs
@@ -5,7 +5,8 @@ use multi_readers::{join_readers, SliceReader};
 fn main() {
     let bytes = b"12345678";
     let bytes1 = b"12345678";
-    let mut reader = join_readers!(SliceReader::new(bytes), SliceReader::new(bytes1));
+    let bytes2 = b"abcd";
+    let mut reader = join_readers!(SliceReader::new(bytes), SliceReader::new(bytes1), SliceReader::new(bytes2));
     reader.set_process_func(|slice| {
         for b in slice {
             *b += 1
@@ -17,5 +18,5 @@ fn main() {
     let len = reader.read(&mut buf).unwrap();
     println!("{}", String::from_utf8_lossy(&buf[..len]));
     let len = reader.read(&mut buf).unwrap();
-    println!("{:?}", String::from_utf8_lossy(&buf[..len]));
+    println!("{}", String::from_utf8_lossy(&buf[..len]));
 }
